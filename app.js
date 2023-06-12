bc9d1378117a06e9806a15452abbac15adc91d7b
Christal Bell <christalvictoriabell@gmail.com>
2023-06-11 18:13:43 -0700

    still working on js for mo v yr price

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-toggle= () => {
-    const basic=document.getElementById("left-container");
-    const professional=document.getElementById("middle-container"); 
-    const master=document.getElementById("right-container"); 
-}
+// toggle= () => {
+//     const basic=document.getElementById("left-container");
+//     const professional=document.getElementById("middle-container"); 
+//     const master=document.getElementById("right-container"); 
+//     const options= ["left-container", "middle-container", "right-container"]
+   
+//    return document.getElementsByClassName("price").style.visibility ="hidden";
+// }
+
+
+const moprice = document.querySelector('.switch .moprice');
+const price = document.querySelector('.switch .price');
