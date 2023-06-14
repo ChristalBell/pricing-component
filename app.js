bcbba365664351f5251697f2c2093c19c105baa1
Christal Bell <christalvictoriabell@gmail.com>
2023-06-13 19:06:17 -0700

    project complete need scss

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-// toggle= () => {
-//     const basic=document.getElementById("left-container");
-//     const professional=document.getElementById("middle-container"); 
-//     const master=document.getElementById("right-container"); 
-//     const options= ["left-container", "middle-container", "right-container"]
-   
-//    return document.getElementsByClassName("price").style.visibility ="hidden";
-// }
-
-
-const moprice = document.querySelector('.switch .moprice');
-const price = document.querySelector('.switch .price');
+const annualPrice =document.getElementsByClassName("annual-price");
+const monthlyPrice =document.getElementsByClassName("monthly-price");
+let active= false;
+const toggle = ()=> {
+    console.log(annualPrice);
+    active = !active
+    if (active){
+        Array.from(annualPrice).forEach((price)=>{
+            price.classList.add("hidden");     
+        })
+        Array.from(monthlyPrice).forEach((price)=>{
+            price.classList.remove("hidden");     
+        })
+    }else {
+        Array.from(monthlyPrice).forEach((price)=>{
+            price.classList.add("hidden");     
+        })
+        Array.from(annualPrice).forEach((price)=>{
+            price.classList.remove("hidden");     
+        })
+    }
+}
